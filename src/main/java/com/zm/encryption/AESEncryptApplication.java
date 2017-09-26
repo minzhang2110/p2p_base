1ee62c22b45068d802267deba62c895b5a7b625a
zhangmin <512444693@qq.com>
2017-09-26 17:47:14 +0800

    update

--- src/main/java/com/zm/encryption/AESEncryptApplication.java
+++ src/main/java/com/zm/encryption/AESEncryptApplication.java
@@ -2,6 +2,11 @@ package com.zm.encryption;
 
 import com.zm.utils.BU;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 /**
  * Created by zhangmin on 2015/11/14.
  */
@@ -16,21 +21,40 @@ public class AESEncryptApplication {
     //需大于等于12个字节
     public static native byte[] decryptEx(byte[] in_buffer, int in_len);
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        if (args.length != 1) {
+            System.out.println("需要文件名作为参数");
+            System.exit(1);
+        }
 
-        String hex = "00  00 00 65 00 00 00 00 00 \n" +
-                "00 00 10 be b2 cd 8e 1f  81 26 37 35 ea 09 ab 76 \n" +
-                "ee c4 fb";
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(new FileReader(args[0]));
+        } catch (FileNotFoundException e) {
+            System.out.println("文件不存在" + args[0]);
+            System.exit(1);
+        }
+        char[] buffer = new char[4096];
+        StringBuffer sb = new StringBuffer();
+        int len;
+        while((len = reader.read(buffer)) != -1) {
+            sb.append(buffer, 0, len);
+        }
+
+        reader.close();
+        String hex = sb.toString();
+
+        if(hex.length() < 24){
+            throw new IllegalStateException("AES解密失败, 原因 : 数据小于12字节");
+        }
         hex = hex.trim().replaceAll("\\s", "");
-        //byte[] one = BU.hex2Bytes(hex);
 
-        //byte[] two = encryptEx(one, one.length);
         byte[] two = BU.hex2Bytes(hex);
         byte[] three = decryptEx(two, two.length);
-
-        //System.out.println(BU.bytes2Hex(one));
-        System.out.println(BU.bytes2HexGoodLook(two));
-        System.out.println("=========================");
-        System.out.println(BU.bytes2HexGoodLook(three));
+        if (three != null) {
+            System.out.println(BU.bytes2HexGoodLook(three));
+        } else {
+            System.out.println("解密失败");
+        }
     }
 }
